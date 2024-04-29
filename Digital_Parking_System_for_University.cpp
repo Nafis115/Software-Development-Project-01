13a94b2b54b0a5e777155694e21ec25a5a5777c2
Nafis115 <20234103116@cse.bubt.edu.bd>
2024-04-29 18:04:31 +0600

    re_arrange

--- Digital_Parking_System_for_University.cpp
+++ Digital_Parking_System_for_University.cpp
@@ -1,30 +1,46 @@
 #include <bits/stdc++.h>
 #include <windows.h>
 using namespace std;
-class User{
-    public:
-    string name;
+bool login = false;
+class User
+{
+public:
+    string email;
     int id;
     string vehicle_id;
-    User(){
-        name=" ";
-        id=0;
-        vehicle_id=" ";
+    User()
+    {
+        email = " ";
+        id = 0;
+        vehicle_id = " ";
     }
-    User(string name,int id, string vehicle_id){
-        this->name=name;
-        this->id=id;
-        this->vehicle_id=vehicle_id;
-    
+    User(string email, int id, string vehicle_id)
+    {
+        this->email = email;
+        this->id = id;
+        this->vehicle_id = vehicle_id;
     }
 };
+void dot_degin()
+{
+    Sleep(650);
+    printf(".");
+    Sleep(1000);
+    printf(".");
+    Sleep(1000);
+    printf(".");
+    Sleep(1000);
+    printf(".");
+    Sleep(1000);
+    printf("\n");
+}
 
-void s_infor(string name, int pass)
+void admin_information(string email, int pass)
 {
     ofstream file("admin.txt", ios::app);
     if (file.is_open())
     {
-        file << name << " " << pass << endl;
+        file << email << " " << pass << endl;
         file.close();
     }
     else
@@ -33,16 +49,16 @@ void s_infor(string name, int pass)
     }
 }
 
-bool check_admin(string name, int pass)
+bool check_admin(string email, int pass)
 {
     ifstream file("admin.txt");
     if (file.is_open())
     {
-        string stored_name;
+        string stored_email;
         int stored_pass;
-        while (file >> stored_name >> stored_pass)
+        while (file >> stored_email >> stored_pass)
         {
-            if (stored_name == name && stored_pass == pass)
+            if (stored_email == email && stored_pass == pass)
             {
                 file.close();
                 return true;
@@ -52,9 +68,6 @@ bool check_admin(string name, int pass)
     }
     return false;
 }
-
-
-
 
 void inter_face()
 {
@@ -70,83 +83,119 @@ void inter_face()
     cout << "Option 1: Registration" << endl;
     cout << "Option 2: Login" << endl;
     cout << "Option 3: Exit" << endl;
-    cout << "\n"  << endl;
+    cout << "\n"
+         << endl;
 }
 
 void registration_method()
 {
     cout << "Welcome to our Registration process: " << endl;
-    string name;
+    string email;
     int pass;
-    cout << "Enter your name: ";
-    cin >> name;
+    cout << "Enter your email: ";
+    cin >> email;
     cout << "Enter your password: ";
     cin >> pass;
-    s_infor(name, pass);
-    printf("Registration is processign.");
-    printf(".");
-    Sleep(1000);
-    printf(".");
-    Sleep(1000);
-    printf(".");
-    Sleep(1000);
-    printf(".");
-    Sleep(1000);
+    admin_information(email, pass);
+    printf("Registration is processing");
+    dot_degin();
     cout << ("\n");
     cout << "Your Registration Successful" << endl;
-    Sleep(1500);
+    Sleep(2000);
     system("CLS");
     inter_face();
-
-    
 }
 
 void login_method()
 {
     cout << "Welcome To Login Page" << endl;
-    string name;
-    cout << "Enter your name: ";
-    cin >> name;
+    string email;
+    cout << "Enter your email: ";
+    cin >> email;
     int pass;
     cout << "Enter your password: ";
     cin >> pass;
-    printf("Please Wait for login.");
-    printf(".");
-    Sleep(1000);
-    printf(".");
-    Sleep(1000);
-    printf(".");
-    Sleep(1000);
-    printf(".");
-    Sleep(1000);
-    printf("\n");
-    if (check_admin(name, pass))
+    printf("Please Wait for login");
+    dot_degin();
+    if (check_admin(email, pass))
     {
         cout << "Login successful" << endl;
-        Sleep(1500);
+        login = true;
+        Sleep(2000);
         system("CLS");
     }
     else
     {
 
-        cout << "Incorrect name or password. Please try again." << endl;
-        Sleep(1500);
+        cout << "Incorrect email or password. Please try again." << endl;
+        Sleep(2000);
     }
-
 }
 
+void admin_panel()
+{
+    cout << "*****************************************" << endl;
+    cout << "      Welcome to Our Admin Panel         " << endl;
+    cout << "*****************************************" << endl;
 
-
-
-
-
-
+    cout << "Please Select an Option:" << endl;
+    cout << "Option 1: Park a Vehicle" << endl;
+    cout << "Option 2: Retrieve a Vehicle" << endl;
+    cout << "Option 3: View Available Parking Spaces in University" << endl;
+    cout << "Option 4: Exit" << endl;
+    cout << "*****************************************" << endl;
+}
+void admin_work_panel()
+{
+    if (login)
+    {
+        while (true)
+        {
+            admin_panel();
+            int opt;
+            cout << "Enter your Option: ";
+            cin >> opt;
+            if (opt == 1)
+            {
+                cout << "processing.";
+                dot_degin();
+                system("CLS");
+            }
+            else if (opt == 2)
+            {
+                cout << "processing.";
+                dot_degin();
+                system("CLS");
+            }
+            else if (opt == 3)
+            {
+                cout << "processing.";
+                dot_degin();
+                system("CLS");
+            }
+            else if (opt == 4)
+            {
+                cout << "You are exiting from Admin panel" << endl;
+                Sleep(2000);
+                system("CLS");
+                break;
+            }
+            else
+            {
+                cout << "Invalid option" << endl;
+                cout << "Please try again" << endl;
+                Sleep(2000);
+                system("CLS");
+            }
+        }
+    }
+}
 
 int main()
 {
     inter_face();
 
-    while (5)
+    while (true)
     {
         cout << "Enter your option: ";
         int op;
@@ -158,22 +207,19 @@ int main()
         else if (op == 2)
         {
             login_method();
+            admin_work_panel();
+            system("CLS");
             inter_face();
-            
         }
         else
         {
-            
+
             cout << "Thanks for visiting our parking system" << endl;
-            Sleep(1500); 
+            Sleep(2000);
             break;
         }
     }
     system("CLS");
-
-
-
-
 
     return 0;
 }
