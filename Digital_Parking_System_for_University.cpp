e8ab1191a60e8ae90906d55aaa6b6fe6a919a29d
Nafis115 <20234103116@cse.bubt.edu.bd>
2024-05-01 14:05:56 +0600

    file changing

--- Digital_Parking_System_for_University.cpp
+++ Digital_Parking_System_for_University.cpp
@@ -2,9 +2,9 @@
 #include <windows.h>
 using namespace std;
 bool login = false;
-const int row = 10;
-const int col = 20;
-int mat[row][col];
+const int ROW = 10;
+const int COL = 20;
+
 void dot_degin()
 {
     Sleep(650);
@@ -125,17 +125,56 @@ void admin_panel()
     cout << "Please Select an Option:" << endl;
     cout << "Option 1: Park a Vehicle" << endl;
     cout << "Option 2: Retrieve a Vehicle" << endl;
-    cout << "Option 3: View Available Parking Spaces in University" << endl;
+    cout << "Option 3: View Parking Spaces in University" << endl;
 
     cout << "Option 4: Exit" << endl;
     cout << "*****************************************" << endl;
 }
-
-void show_parking_space()
+void save_parking_space(int mat[ROW][COL])
 {
-    for (int i = 0; i < row; i++)
+    ofstream save_space("save_space.txt");
+    if (save_space.is_open())
     {
-        for (int j = 0; j < col; j++)
+        for (int i = 0; i < ROW; i++)
+        {
+            for (int j = 0; j < COL; j++)
+            {
+                save_space << mat[i][j] << " ";
+            }
+            save_space << endl;
+        }
+        save_space.close();
+    }
+    else
+    {
+        cout << "Can not save parking space" << endl;
+    }
+}
+
+void load_space(int mat[ROW][COL])
+{
+    ifstream load_space("save_space.txt");
+    if (load_space.is_open())
+    {
+        for (int i = 0; i < ROW; i++)
+        {
+            for (int j = 0; j < COL; j++)
+            {
+                load_space >> mat[i][j];
+            }
+        }
+        load_space.close();
+    }
+    else
+    {
+        cout << "Error to open file for load save parking space" << endl;
+    }
+}
+void show_parking_space(int mat[ROW][COL])
+{
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = 0; j < COL; j++)
         {
             if (mat[i][j] == 0)
             {
@@ -151,121 +190,104 @@ void show_parking_space()
         cout << endl;
     }
 }
-void park_a_vehicle(int id, int vehicle_id, int r, int c)
-{
+void park_a_vehicle(int id, int vehicle_id, int r, int c, int mat[ROW][COL]) {
     bool flag = false;
-    if (r >= 0 && r < row && c >= 0 && c < col)
-    {
-        if (mat[r][c] == 0)
-        {
+    if (r >= 0 && r < ROW && c >= 0 && c < COL) {
+        if (mat[r][c] == 0) {
             mat[r][c] = 1;
-
             flag = true;
+            save_parking_space(mat);
         }
-        else
-        {
-            cout << "The parking space Already Booked" << endl;
+        else {
+            cout << "The parking space is already booked" << endl;
         }
     }
-    if (flag == true)
-    {
-        ofstream store_space("parking_space.txt", ios::app);
-        if (store_space.is_open())
-        {
+    if (flag) {
+        ofstream store_space("parking_details.txt", ios::app);
+        if (store_space.is_open()) {
             store_space << vehicle_id << " " << r << " " << c << endl;
         }
         srand(time(nullptr));
         int coupon_num;
         coupon_num = rand();
         ofstream owner("Owner_details.txt", ios::app);
-        if (owner.is_open())
-        {
+        if (owner.is_open()) {
             owner << id << " " << coupon_num << endl;
             cout << "Please print this Coupon Number: " << coupon_num << endl;
-            cout << "And give to the student for again verified" << endl;
+            cout << "And give to the student for verification" << endl;
         }
-        else
-        {
-            cout << "Unable to open to save owner details" << endl;
+        else {
+            cout << "Unable to open file to save owner details" << endl;
         }
         owner.close();
-        cout << "The vehicle Parked this space(" << r << "," << c << ")"
-             << "successfully" << endl;
+        cout << "The vehicle is parked at space (" << r << "," << c << ") successfully" << endl;
     }
 }
 
-void space_finder(int vehicle_id)
-{
-    bool flag = false;
-    ifstream space_finder("parking_space.txt");
+bool space_finder(int vehicle_id) {
+    ifstream space_finder("parking_details.txt");
+    ofstream temp("temp.txt");
     int s_vehicle_id, s_r, s_c;
-    if (space_finder.is_open())
-    {
-
-        while (space_finder >> s_vehicle_id >> s_r >> s_c)
-        {
-            if (vehicle_id == s_vehicle_id)
-            {
-                flag = true;
-                cout << "Enter this Space(" << s_r << "," << s_c << ")"
-                     << "for this vehicle" << endl;
+    bool found = false;
+    if (space_finder.is_open() && temp.is_open()) {
+        while (space_finder >> s_vehicle_id >> s_r >> s_c) {
+            if (vehicle_id == s_vehicle_id) {
+                cout << "Enter this Space(" << s_r << " " << s_c << ") for this vehicle" << endl << endl;
+                found = true;
+            }
+            else {
+                temp << s_vehicle_id << " " << s_r << " " << s_c << endl;
             }
         }
     }
-    else
-    {
-        cout << "Error: can not open file" << endl;
+    else {
+        cout << "Error: cannot open file" << endl;
+        return false;
     }
-
-    if (flag == false)
-    {
-        cout << endl;
-        cout << "You Enter wrong Vehicle Id" << endl;
-        cout << "Please try again valid Vehicle Id" << endl;
+    space_finder.close();
+    temp.close();
+    remove("parking_details.txt");
+    rename("temp.txt", "parking_details.txt");
+    if (!found) {
+        cout << "You entered a wrong Vehicle Id. Please try again with a valid Vehicle Id." << endl;
     }
+    return found;
 }
 
-void retrive_vehicle(int id, int coupon_num, int r, int c)
-{
-    bool flag = false;
-    ifstream owner_ver("Owner_details.txt");
+bool retrieve_vehicle(int id, int coupon_num) {
+    ifstream owner_details("Owner_details.txt");
+    ofstream temp("temp.txt");
     int s_id, s_coupon_num;
-    if (owner_ver.is_open())
-    {
-        while (owner_ver >> s_id >> s_coupon_num)
-        {
-            if (s_id == id)
-            {
-                flag = true;
-            }
-            else
-            {
-                cout << "Wrong id" << endl;
-                cout << "Please try again with valid id" << endl;
-            }
-        }
-
-        if (flag)
-        {
-            if (coupon_num == s_coupon_num)
-            {
-                if (mat[r][c] == 1)
-                {
-                    mat[r][c] = 0;
-                    cout << "Your car retrieve successully" << endl;
-                }
-                else
-                {
-                    cout << "The parking space is empty" << endl;
-                    cout << "Please Enter Valid space" << endl;
+    bool found = false;
+    if (owner_details.is_open() && temp.is_open()) {
+        while (owner_details >> s_id >> s_coupon_num) {
+            if (s_id == id) {
+                if (coupon_num == s_coupon_num) {
+                    found = true;
+                    continue;
                 }
             }
+            temp << s_id << " " << s_coupon_num << endl;
         }
     }
+    else {
+        cout << "Cannot open file to retrieve vehicle" << endl;
+        return false;
+    }
+    owner_details.close();
+    temp.close();
+    remove("Owner_details.txt");
+    rename("temp.txt", "Owner_details.txt");
+    if (!found) {
+        cout << "Wrong id or coupon number. Please try again with valid credentials." << endl;
+    }
+    return found;
 }
+
 int main()
 {
-    mat[10][20] = {0};
+    int mat[ROW][COL] = {0};
+    load_space(mat);
     inter_face();
 
     while (true)
@@ -301,31 +323,48 @@ int main()
                         cin >> type;
                         cout << "Enter Vehicle Id: ";
                         cin >> v_id;
-                        show_parking_space();
-                        cout << "Enter parking space(ex:(r,c)): ";
+                        show_parking_space(mat);
+                        cout << "Enter parking space(ex:(r c)): ";
                         cin >> r >> c;
 
-                        park_a_vehicle(o_id, v_id, r, c);
-                        
+                        park_a_vehicle(o_id, v_id, r, c, mat);
                     }
                     else if (opt == 2)
                     {
 
-                        int o_id, v_id, r, c, c_n;
+                        int owner_id, vehicle_id, r, c, coupon_num;
                         cout << "Enter Vehicle Owner Id: ";
-                        cin >> o_id;
+                        cin >> owner_id;
                         cout << "Enter Vehicle Id: ";
-                        cin >> v_id;
-                        space_finder(v_id);
-                        cout << "Enter parking space Where you park this Vehicle(ex:(r,c)): ";
-                        cin >> r >> c;
-                        cout << "Enter the unique coupon id: ";
-                        cin >> c_n;
-                        retrive_vehicle(o_id, c_n, r, c);
+                        cin >> vehicle_id;
+                        if (space_finder(vehicle_id))
+                        {   cout<<endl;
+                            cout << "Enter parking space Where you park this Vehicle(ex:(r c)): ";
+                            cin >> r >> c;
+                            cout << "Enter the unique coupon id: ";
+                            cin >> coupon_num;
+                            if (retrieve_vehicle(owner_id, coupon_num))
+                            {
+                                if (r >= 0 && r < ROW && c >= 0 && c < COL)
+                                {
+                                    if (mat[r][c] == 1)
+                                    {
+                                        mat[r][c] = 0;
+                                        save_parking_space(mat);
+                                        cout << "Vehicle retrieve successfull" << endl;
+                                    }
+                                    else
+                                    {
+                                        cout << "This space(" << r << " " << c << ")"
+                                             << "is already Booked" << endl;
+                                    }
+                                }
+                            }
+                        }
                     }
                     else if (opt == 3)
                     {
-                        show_parking_space();
+                        show_parking_space(mat);
                     }
                     else if (opt == 4)
                     {
